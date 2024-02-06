113e570282097c999601546e18105caa22d93f4e
THANUKRISHNA <THANUKRISHNA@users.noreply.github.com>
2024-02-06 08:34:37 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,13 @@
+'''import area
+print("area of square",area.square(2))
+
+from area import*
+print(rectangle(2,4))
+print(square(2))
+
+from area import *
+print(calculator(2,4))'''
+
+#calling packages 
+from packages.cal import *
+print(calculator(2,4))
